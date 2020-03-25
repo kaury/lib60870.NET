3c05ea474d6650f5a28a4a01f54c7e9a8a6cf4c0
kaury <kaury@163.com>
2020-03-25 11:19:01 +0800

    debug examples cs104-client1

--- examples/cs104-client1/Program.cs
+++ examples/cs104-client1/Program.cs
@@ -7,190 +7,292 @@ using lib60870.CS104;
 
 namespace cs104_client1
 {
-	class MainClass
-	{
+    class MainClass
+    {
 
-		private static void ConnectionHandler (object parameter, ConnectionEvent connectionEvent)
-		{
-			switch (connectionEvent) {
-			case ConnectionEvent.OPENED:
-				Console.WriteLine ("Connected");
-				break;
-			case ConnectionEvent.CLOSED:
-				Console.WriteLine ("Connection closed");
-				break;
-			case ConnectionEvent.STARTDT_CON_RECEIVED:
-				Console.WriteLine ("STARTDT CON received");
-				break;
-			case ConnectionEvent.STOPDT_CON_RECEIVED:
-				Console.WriteLine ("STOPDT CON received");
-				break;
-			}
-		}
+        private static void ConnectionHandler(object parameter, ConnectionEvent connectionEvent)
+        {
+            switch (connectionEvent)
+            {
+                case ConnectionEvent.OPENED:
+                    Console.WriteLine("Connected");
+                    break;
+                case ConnectionEvent.CLOSED:
+                    Console.WriteLine("Connection closed");
+                    break;
+                case ConnectionEvent.STARTDT_CON_RECEIVED:
+                    Console.WriteLine("STARTDT CON received");
+                    break;
+                case ConnectionEvent.STOPDT_CON_RECEIVED:
+                    Console.WriteLine("STOPDT CON received");
+                    break;
+            }
+        }
 
-		private static bool asduReceivedHandler(object parameter, ASDU asdu)
-		{
-			Console.WriteLine (asdu.ToString ());
+        private static bool asduReceivedHandler(object parameter, ASDU asdu)
+        {
+            Console.WriteLine(asdu.ToString());
 
-			if (asdu.TypeId == TypeID.M_SP_NA_1) {
+            if (asdu.TypeId == TypeID.M_SP_NA_1)
+            {
 
-				for (int i = 0; i < asdu.NumberOfElements; i++) {
+                for (int i = 0; i < asdu.NumberOfElements; i++)
+                {
 
-					var val = (SinglePointInformation)asdu.GetElement (i);
+                    var val = (SinglePointInformation)asdu.GetElement(i);
 
-					Console.WriteLine ("  IOA: " + val.ObjectAddress + " SP value: " + val.Value);
-					Console.WriteLine ("   " + val.Quality.ToString ());
-				}
-			} else if (asdu.TypeId == TypeID.M_ME_TE_1) {
-			
-				for (int i = 0; i < asdu.NumberOfElements; i++) {
+                    Console.WriteLine("  IOA: " + val.ObjectAddress + " SP value: " + val.Value);
+                    Console.WriteLine("   " + val.Quality.ToString());
+                }
+            }
+            else if (asdu.TypeId == TypeID.M_ME_TE_1)
+            {
 
-					var msv = (MeasuredValueScaledWithCP56Time2a)asdu.GetElement (i);
+                for (int i = 0; i < asdu.NumberOfElements; i++)
+                {
 
-					Console.WriteLine ("  IOA: " + msv.ObjectAddress + " scaled value: " + msv.ScaledValue);
-					Console.WriteLine ("   " + msv.Quality.ToString ());
-					Console.WriteLine ("   " + msv.Timestamp.ToString ());
-				}
+                    var msv = (MeasuredValueScaledWithCP56Time2a)asdu.GetElement(i);
 
-			} else if (asdu.TypeId == TypeID.M_ME_TF_1) {
+                    Console.WriteLine("  IOA: " + msv.ObjectAddress + " scaled value: " + msv.ScaledValue);
+                    Console.WriteLine("   " + msv.Quality.ToString());
+                    Console.WriteLine("   " + msv.Timestamp.ToString());
+                }
 
-				for (int i = 0; i < asdu.NumberOfElements; i++) {
-					var mfv = (MeasuredValueShortWithCP56Time2a)asdu.GetElement (i);
+            }
+            else if (asdu.TypeId == TypeID.M_ME_TF_1)
+            {
 
-					Console.WriteLine ("  IOA: " + mfv.ObjectAddress + " float value: " + mfv.Value);
-					Console.WriteLine ("   " + mfv.Quality.ToString ());
-					Console.WriteLine ("   " + mfv.Timestamp.ToString ());
-					Console.WriteLine ("   " + mfv.Timestamp.GetDateTime ().ToString ());
-				}
-			} else if (asdu.TypeId == TypeID.M_SP_TB_1) {
+                for (int i = 0; i < asdu.NumberOfElements; i++)
+                {
+                    var mfv = (MeasuredValueShortWithCP56Time2a)asdu.GetElement(i);
 
-				for (int i = 0; i < asdu.NumberOfElements; i++) {
+                    Console.WriteLine("  IOA: " + mfv.ObjectAddress + " float value: " + mfv.Value);
+                    Console.WriteLine("   " + mfv.Quality.ToString());
+                    Console.WriteLine("   " + mfv.Timestamp.ToString());
+                    Console.WriteLine("   " + mfv.Timestamp.GetDateTime().ToString());
+                }
+            }
+            else if (asdu.TypeId == TypeID.M_SP_TB_1)
+            {
 
-					var val = (SinglePointWithCP56Time2a)asdu.GetElement (i);
+                for (int i = 0; i < asdu.NumberOfElements; i++)
+                {
 
-					Console.WriteLine ("  IOA: " + val.ObjectAddress + " SP value: " + val.Value);
-					Console.WriteLine ("   " + val.Quality.ToString ());
-					Console.WriteLine ("   " + val.Timestamp.ToString ());
-				}
-			} else if (asdu.TypeId == TypeID.M_ME_NC_1) {
+                    var val = (SinglePointWithCP56Time2a)asdu.GetElement(i);
 
-				for (int i = 0; i < asdu.NumberOfElements; i++) {
-					var mfv = (MeasuredValueShort)asdu.GetElement (i);
+                    Console.WriteLine("  IOA: " + val.ObjectAddress + " SP value: " + val.Value);
+                    Console.WriteLine("   " + val.Quality.ToString());
+                    Console.WriteLine("   " + val.Timestamp.ToString());
+                }
+            }
+            else if (asdu.TypeId == TypeID.M_ME_NC_1)
+            {
 
-					Console.WriteLine ("  IOA: " + mfv.ObjectAddress + " float value: " + mfv.Value);
-					Console.WriteLine ("   " + mfv.Quality.ToString ());
-				}
-			} else if (asdu.TypeId == TypeID.M_ME_NB_1) {
+                for (int i = 0; i < asdu.NumberOfElements; i++)
+                {
+                    var mfv = (MeasuredValueShort)asdu.GetElement(i);
 
-				for (int i = 0; i < asdu.NumberOfElements; i++) {
+                    Console.WriteLine("  IOA: " + mfv.ObjectAddress + " float value: " + mfv.Value);
+                    Console.WriteLine("   " + mfv.Quality.ToString());
+                }
+            }
+            else if (asdu.TypeId == TypeID.M_ME_NB_1)
+            {
 
-					var msv = (MeasuredValueScaled)asdu.GetElement (i);
+                for (int i = 0; i < asdu.NumberOfElements; i++)
+                {
 
-					Console.WriteLine ("  IOA: " + msv.ObjectAddress + " scaled value: " + msv.ScaledValue);
-					Console.WriteLine ("   " + msv.Quality.ToString ());
-				}
+                    var msv = (MeasuredValueScaled)asdu.GetElement(i);
 
-			} else if (asdu.TypeId == TypeID.M_ME_ND_1) {
+                    Console.WriteLine("  IOA: " + msv.ObjectAddress + " scaled value: " + msv.ScaledValue);
+                    Console.WriteLine("   " + msv.Quality.ToString());
+                }
 
-				for (int i = 0; i < asdu.NumberOfElements; i++) {
+            }
+            else if (asdu.TypeId == TypeID.M_ME_ND_1)
+            {
 
-					var msv = (MeasuredValueNormalizedWithoutQuality)asdu.GetElement (i);
+                for (int i = 0; i < asdu.NumberOfElements; i++)
+                {
 
-					Console.WriteLine ("  IOA: " + msv.ObjectAddress + " scaled value: " + msv.NormalizedValue);
-				}
+                    var msv = (MeasuredValueNormalizedWithoutQuality)asdu.GetElement(i);
 
-			} else if (asdu.TypeId == TypeID.C_IC_NA_1) {
-				if (asdu.Cot == CauseOfTransmission.ACTIVATION_CON)
-					Console.WriteLine ((asdu.IsNegative ? "Negative" : "Positive") + "confirmation for interrogation command");
-				else if (asdu.Cot == CauseOfTransmission.ACTIVATION_TERMINATION)
-					Console.WriteLine ("Interrogation command terminated");
-			} else if (asdu.TypeId == TypeID.F_DR_TA_1) {
-				Console.WriteLine ("Received file directory:\n------------------------");
-				int ca = asdu.Ca;
+                    Console.WriteLine("  IOA: " + msv.ObjectAddress + " scaled value: " + msv.NormalizedValue);
+                }
 
-				for (int i = 0; i < asdu.NumberOfElements; i++) {
-					FileDirectory fd = (FileDirectory)asdu.GetElement (i);
+            }
+            else if (asdu.TypeId == TypeID.C_IC_NA_1)
+            {
+                if (asdu.Cot == CauseOfTransmission.ACTIVATION_CON)
+                    Console.WriteLine((asdu.IsNegative ? "Negative" : "Positive") + "confirmation for interrogation command");
+                else if (asdu.Cot == CauseOfTransmission.ACTIVATION_TERMINATION)
+                    Console.WriteLine("Interrogation command terminated");
+            }
+            else if (asdu.TypeId == TypeID.F_DR_TA_1)
+            {
+                Console.WriteLine("Received file directory:\n------------------------");
+                int ca = asdu.Ca;
 
-					Console.Write (fd.FOR ? "DIR:  " : "FILE: ");
+                for (int i = 0; i < asdu.NumberOfElements; i++)
+                {
+                    FileDirectory fd = (FileDirectory)asdu.GetElement(i);
 
-					Console.WriteLine ("CA: {0} IOA: {1} Type: {2}", ca, fd.ObjectAddress, fd.NOF.ToString ()); 
-				}
+                    Console.Write(fd.FOR ? "DIR:  " : "FILE: ");
 
-			}
-			else {
-				Console.WriteLine ("Unknown message type!");
-			}
+                    Console.WriteLine("CA: {0} IOA: {1} Type: {2}", ca, fd.ObjectAddress, fd.NOF.ToString());
+                }
 
-			return true;
-		}
+            }
+            else if (asdu.TypeId == TypeID.C_RD_NA_1)//102
+            {
+                if (asdu.NumberOfElements > 0)
+                {
+                    var obj = (ReadCommand)asdu.GetElement(0);
+                    Console.WriteLine($"Read command: ObjectAddress:{obj.ObjectAddress} !");
+                }
+            }
+            else if (asdu.TypeId == TypeID.C_CS_NA_1)
+            {
+                var obj = (ClockSynchronizationCommand)asdu.GetElement(0);
+                if (asdu.Cot == CauseOfTransmission.REQUEST)
+                {
+                    Console.WriteLine((asdu.IsNegative ? "Negative" : "Positive") + " confirmation for clock read command. " + obj.NewTime.ToString());
+                }
+                else if (asdu.Cot == CauseOfTransmission.ACTIVATION_CON)
+                {
+                    Console.WriteLine((asdu.IsNegative ? "Negative" : "Positive") + " confirmation for clock synchronization command");
+                }
+                else if (asdu.Cot == CauseOfTransmission.ACTIVATION_TERMINATION)
+                {
+                    Console.WriteLine("clock synchronization command terminated");
+                }
+            }
+            else if (asdu.TypeId == TypeID.C_RR_NA_1)
+            {
+                if (asdu.NumberOfElements > 0)
+                {
+                    var obj = (ReadCurrentSettingArea)asdu.GetElement(0);
+                    Console.WriteLine($"Read current setting area: SN:{obj.SNCurrent} SN min:{obj.SNMin} SN max:{obj.SNMax}!");
+                }
+            }
+            else if (asdu.TypeId == TypeID.C_RS_NA_1)
+            {
+                if (asdu.NumberOfElements > 0)
+                {
+                    var obj = (ReadParameters)asdu.GetElement(0);
+                    Console.WriteLine($" SN:{obj.SN} PI:{obj.PI.Followup} {obj.PI.RES} {obj.PI.CR} {obj.PI.Select}");
+                }
+            }
+            else if (asdu.TypeId == TypeID.M_EI_NA_1)
+            {
 
-		public class Receiver : IFileReceiver 
-		{
-			public void Finished(FileErrorCode result)
-			{ 
-				Console.WriteLine ("File download finished - code: " + result.ToString ());
-			}
+            }
+            else
+            {
+                Console.WriteLine("Unknown message type!");
+            }
+
+            return true;
+        }
+
+        public class Receiver : IFileReceiver
+        {
+            public void Finished(FileErrorCode result)
+            {
+                Console.WriteLine("File download finished - code: " + result.ToString());
+            }
 
 
-			public void SegmentReceived(byte sectionName, int offset, int size, byte[] data)
-			{
-				Console.WriteLine ("File segment - sectionName: {0} offset: {1} size: {2}", sectionName, offset, size);
-			}
-		}
+            public void SegmentReceived(byte sectionName, int offset, int size, byte[] data)
+            {
+                Console.WriteLine("File segment - sectionName: {0} offset: {1} size: {2}", sectionName, offset, size);
+            }
 
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Using lib60870.NET version " + LibraryCommon.GetLibraryVersionString ());
+            public void SegmentReceived(int fileId, int offset, uint size, byte[] data)
+            {
+                Console.WriteLine("File segment - fileId: {0} offset: {1} size: {2}", fileId, offset, size);
+            }
+        }
+        static Connection con;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Using lib60870.NET version " + LibraryCommon.GetLibraryVersionString());
+            Console.WriteLine("IP:");
+            string ip = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(ip)) ip = "192.168.1.11";
+            con = new Connection(ip);
 
-			Connection con = new Connection ("192.168.1.2");
+            con.DebugOutput = true;
 
-			con.DebugOutput = false;
+            con.SetASDUReceivedHandler(asduReceivedHandler, null);
+            con.SetConnectionHandler(ConnectionHandler, null);
 
-			con.SetASDUReceivedHandler (asduReceivedHandler, null);
-			con.SetConnectionHandler (ConnectionHandler, null);
+            con.Connect();
 
-            con.Connect ();
+            Thread.Sleep(5000);
 
-			con.GetDirectory (1);
+            con.SendInterrogationCommand(CauseOfTransmission.ACTIVATION, 1, QualifierOfInterrogation.STATION);
+            
+            Console.ReadKey();
 
-			con.GetFile (1, 30000, NameOfFile.TRANSPARENT_FILE, new Receiver ());
+            con.GetFile(1, 0, "soe", new Receiver());
 
-            Thread.Sleep (5000);
+            con.SendReadCommand(1, 0x4001);
+            Thread.Sleep(1000);
 
-			con.SendTestCommand (1);
+            con.ReadParameters(1, 0, new int[] { 0x8001 });
+            Thread.Sleep(1000);
 
-			con.SendInterrogationCommand (CauseOfTransmission.ACTIVATION, 1, QualifierOfInterrogation.STATION);
+            con.SendReadClockCommand(1);
+            Thread.Sleep(1000);
 
-			Thread.Sleep (5000);
+            con.ReadCurrentSettingArea(1);
 
-			con.SendControlCommand (CauseOfTransmission.ACTIVATION, 1, new SingleCommand (5000, true, false, 0));
 
-			con.SendControlCommand (CauseOfTransmission.ACTIVATION, 1, new DoubleCommand (5001, DoubleCommand.ON, false, 0));
+            Thread.Sleep(5000);
+            Console.WriteLine("---------------------"); 
+            Console.ReadKey();
 
-			con.SendControlCommand (CauseOfTransmission.ACTIVATION, 1, new StepCommand (5002, StepCommandValue.HIGHER, false, 0));
+            con.GetDirectory(1);
 
-			con.SendControlCommand (CauseOfTransmission.ACTIVATION, 1, 
-			                        new SingleCommandWithCP56Time2a (5000, false, false, 0, new CP56Time2a (DateTime.Now)));
+            con.GetFile(1, 30000, NameOfFile.TRANSPARENT_FILE, new Receiver());
 
-			/* Synchronize clock of the controlled station */
-			con.SendClockSyncCommand (1 /* CA */, new CP56Time2a (DateTime.Now)); 
+            Thread.Sleep(5000);
 
-			Console.WriteLine ("CLOSE");
+            con.SendTestCommand(1);
 
-			con.Close ();
+            con.SendInterrogationCommand(CauseOfTransmission.ACTIVATION, 1, QualifierOfInterrogation.STATION);
 
-			Console.WriteLine ("RECONNECT");
+            Thread.Sleep(5000);
 
-			con.Connect ();
+            con.SendControlCommand(CauseOfTransmission.ACTIVATION, 1, new SingleCommand(5000, true, false, 0));
 
-			Thread.Sleep (5000);
+            con.SendControlCommand(CauseOfTransmission.ACTIVATION, 1, new DoubleCommand(5001, DoubleCommand.ON, false, 0));
 
-            Console.WriteLine ("CLOSE 2");
+            con.SendControlCommand(CauseOfTransmission.ACTIVATION, 1, new StepCommand(5002, StepCommandValue.HIGHER, false, 0));
 
-            con.Close ();
+            con.SendControlCommand(CauseOfTransmission.ACTIVATION, 1,
+                                    new SingleCommandWithCP56Time2a(5000, false, false, 0, new CP56Time2a(DateTime.Now)));
 
-			Console.WriteLine("Press any key to terminate...");
-			Console.ReadKey();
-		}
-	}
+            /* Synchronize clock of the controlled station */
+            con.SendClockSyncCommand(1 /* CA */, new CP56Time2a(DateTime.Now));
+
+            Console.WriteLine("CLOSE");
+
+            con.Close();
+
+            Console.WriteLine("RECONNECT");
+
+            con.Connect();
+
+            Thread.Sleep(5000);
+
+            Console.WriteLine("CLOSE 2");
+
+            con.Close();
+
+            Console.WriteLine("Press any key to terminate...");
+            Console.ReadKey();
+        }
+    }
 }
